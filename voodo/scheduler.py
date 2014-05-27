9f2c7d1597846cd45dc5abe839a67fda1ea63b20
ToeJam <toejamhoney@gmail.com>
2014-05-27 11:51:05 -0400

    Renamed comm, errors, and parser to shorter names

--- voodo/scheduler.py
+++ voodo/scheduler.py
@@ -8,8 +8,8 @@ from multiprocessing import Process
 import vm_mgmt
 import tasks
 
-class Scheduler(object):
 
+class Scheduler(object):
     def __init__(self, storage_gateway, vm_map, proxy):
         self.storage = storage_gateway
         self.loop_thread = None
@@ -23,7 +23,7 @@ class Scheduler(object):
         self.stop_flag.clear()
         self.loop_thread = threading.Thread(target=self.job_loop)
         self.loop_thread.start()
-      
+
     def stop(self):
         self.stop_flag.set()
         try:
@@ -34,10 +34,10 @@ class Scheduler(object):
         else:
             if self.loop_thread.is_alive():
                 self.logger.error("Scheduler.stop(): Unable to stop the schedulers analysis loop immediately.")
-            
+
     def job_loop(self):
         threads = []
-        self.logger.info('Scheduler begin job loop') 
+        self.logger.info('Scheduler begin job loop')
 
         while not self.stop_flag.is_set():
             job_dic = self.proxy.job.get_a_task()
@@ -63,21 +63,21 @@ class Scheduler(object):
                 job_dic['err_path'] = err
                 '''
                 self.logger.debug("Scheduler.job_loop creating task thread")
-                threads.append( threading.Thread(target=self.task_handle, args=(test_vm, task) ) )
-                self.proxy.job.begin_task( (job_dic['job_id'], job_dic['sample_id']) )
+                threads.append(threading.Thread(target=self.task_handle, args=(test_vm, task)))
+                self.proxy.job.begin_task((job_dic['job_id'], job_dic['sample_id']))
                 self.logger.debug("Scheduler.job_loop marked task as begun. Starting task thread")
                 threads[-1].start()
-        
+
         for thread in threads:
             self.logger.debug('Scheduler job loop joining thread: ' + str(thread))
             thread.join(1)
 
         self.logger.info('Scheduler completed job loop')
-          
+
     def task_handle(self, virt_dev, task):
         if task.proceed:
             self.logger.debug("Scheduler.task_handle creating task process")
-            task_child = Process(target=virt_dev.run_task, args = (task, ) )
+            task_child = Process(target=virt_dev.run_task, args=(task, ))
             self.logger.debug("Scheduler.task_handle starting process")
             task_child.start()
             task_child.join(90)
@@ -89,7 +89,7 @@ class Scheduler(object):
             task.log("Task error before process creation. Aborting")
         self.vm_pool.release(virt_dev.name)
 
-    
+
 '''
 if __name__ == '__main__':
     import vbox
--- voodo/v_parser.py
+++ voodo/v_parser.py
@@ -13,19 +13,18 @@ try:
     import config
 except ImportError:
     class config:
-        SETTINGS = { 'LOG_USER': 'logger' }
+        SETTINGS = {'LOG_USER': 'logger'}
 
 
 class BaseParser(object):
-
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-    
+
     def pre_parse(self, line):
         if not isinstance(line, list):
             line = line.split(' ')
         return line
-    
+
     def post_parse(self, dic):
         return dic
 
@@ -57,11 +56,11 @@ class BaseParser(object):
 
 
 class VoodoParser(BaseParser):
-    
     def __init__(self):
         super(VoodoParser, self).__init__()
         self.parser.add_argument('-i', '--input', nargs='*', default='', help='Input file to replace stdin')
-        self.parser.add_argument('-d', '--debug', default='INFO', help='Indicates debug level. CRITICAL, ERROR, WARNING (default), INFO, or DEBUG')
+        self.parser.add_argument('-d', '--debug', default='INFO',
+                                 help='Indicates debug level. CRITICAL, ERROR, WARNING (default), INFO, or DEBUG')
 
     def post_parse(self, dic):
         file_in = dic.get('input')
@@ -69,8 +68,8 @@ class VoodoParser(BaseParser):
             dic['input'] = self.sanitize_path(' '.join(file_in))
         return dic
 
-class VBoxParser(BaseParser):
 
+class VBoxParser(BaseParser):
     def __init__(self):
         super(VBoxParser, self).__init__()
         self.parser.add_argument('-d', '--desc', default='none', help='TODO')
@@ -87,51 +86,58 @@ class VBoxParser(BaseParser):
 
 
 class JobParser(BaseParser):
-    
     def __init__(self):
         super(JobParser, self).__init__()
         self.parser.add_argument('-a', '--analyzers', nargs='*', default=[],
-                            help='optional: specify analyzing tools to be run before and after launch. Options = autorunsc, registry')
+                                 help='optional: specify analyzing tools to be run before and after launch. Options = '
+                                      'autorunsc, registry')
         self.parser.add_argument('-b', '--before', action='store_true', default=False,
-                            help='optional: specify this job as creating a before analysis image used in diffs later')
+                                 help='optional: specify this job as creating a before analysis image used in diffs '
+                                      'later')
         self.parser.add_argument('--date_stamp', default=datetime.today().strftime('%Y-%m-%d'))
         self.parser.add_argument('-d', '--diff', action='store_true', default=False,
-                            help='optional: specify this job should have a diff run on the logs')
+                                 help='optional: specify this job should have a diff run on the logs')
         self.parser.add_argument('-f', '--flag', nargs='*', default=[],
-                            help='optional: flags for exe')
+                                 help='optional: flags for exe')
         self.parser.add_argument('--flag2', nargs='*', default=[],
-                            help='optional: flags for exe')
+                                 help='optional: flags for exe')
         self.parser.add_argument('--flag3', nargs='*', default=[],
-                            help='optional: flags for exe')
-        self.parser.add_argument('-i', '--input', nargs='*', default = [],
-                            help='optional: use with a tool to specify an input file name')
+                                 help='optional: flags for exe')
+        self.parser.add_argument('-i', '--input', nargs='*', default=[],
+                                 help='optional: use with a tool to specify an input file name')
         self.parser.add_argument('-j', '--job', default='no-name_' + datetime.today().strftime('%I-%M-%S'),
-                            help='optional: name for the job that will be run')
+                                 help='optional: name for the job that will be run')
         self.parser.add_argument('-l', '--log', action='store_true', default=False,
-                            help='optional: name of log, or white space separated list of log names (from config.py). No entry = no logs')
-        self.parser.add_argument('--lib', default='', help='optional: Specifies a library name (source of sample sets/catalogs)')
+                                 help='optional: name of log, or white space separated list of log names (from config'
+                                      '.py). No entry = no logs')
+        self.parser.add_argument('--lib', default='',
+                                 help='optional: Specifies a library name (source of sample sets/catalogs)')
         self.parser.add_argument('-m', '--method', default='debug', help='RPC Method to run on guest')
-        self.parser.add_argument('-o', '--output', nargs='*', default = [],
-                            help='optional: use with a tool to specify an output file name')
-        self.parser.add_argument('--path', default='', help='optional: Set the path for a library or catalog, for example')
+        self.parser.add_argument('-o', '--output', nargs='*', default=[],
+                                 help='optional: use with a tool to specify an output file name')
+        self.parser.add_argument('--path', default='',
+                                 help='optional: Set the path for a library or catalog, for example')
         self.parser.add_argument('-p', '--priority', type=int, default=2,
-                            help='optional: set the priority of the jobs being forked. High = 0, Med(Default) = 1, Low = 2')
+                                 help='optional: set the priority of the jobs being forked. High = 0, Med(Default) = '
+                                      '1, Low = 2')
         self.parser.add_argument('--push_sample', default=False, action='store_true')
         self.parser.add_argument('-r', '--reset', action='store_true', default=False,
-                            help='optional: default is true, tells the driver to reset(kill, restore, start) the guests between forked procs')
+                                 help='optional: default is true, tells the driver to reset(kill, restore, '
+                                      'start) the guests between forked procs')
         self.parser.add_argument('--results', default={})
         self.parser.add_argument('-s', '--set', default='',
-                            help='optional: Specify a sample set name. A set is a catalog, or sample set of artifacts')
+                                 help='optional: Specify a sample set name. A set is a catalog, or sample set of '
+                                      'artifacts')
         self.parser.add_argument('-t', '--pin_tool', nargs='*', default=[],
-                            help='optional: dll to be used with pin execution')
+                                 help='optional: dll to be used with pin execution')
         self.parser.add_argument('--time_stamp', default=datetime.today().strftime('%I-%M'))
         self.parser.add_argument('--username', default=config.SETTINGS['LOG_USER'])
         self.parser.add_argument('-v', '--vms', nargs='*', default=[],
-                            help='list of virtual machine names to run job on')
+                                 help='list of virtual machine names to run job on')
         self.parser.add_argument('-w', '--wait', type=int, default=10,
-                            help='optional argument for a wait time. default is 10')
+                                 help='optional argument for a wait time. default is 10')
         self.parser.add_argument('-x', '--exe', nargs='*', default=[],
-                            help='required for launch command: path to file to load/run')
+                                 help='required for launch command: path to file to load/run')
 
     def post_parse(self, dic):
         dic['exe'] = ' '.join(dic['exe'])
@@ -155,13 +161,13 @@ if __name__ == '__main__':
         print 'Test VBoxParser input (q to quit):',
         args = raw_input().split(' ')
         if args[0] == 'q':
-          break
+            break
         print 'VBOXPARSER:', parser.parse_args(args)
 
     parser2 = JobParser()
     while True:
-      print 'Test JobParser input (q to quit):',
-      args = raw_input().split(' ')
-      if args[0] == 'q':
-        break
-      print 'JOBPARSER:', parser2.parse_args(args)
+        print 'Test JobParser input (q to quit):',
+        args = raw_input().split(' ')
+        if args[0] == 'q':
+            break
+        print 'JOBPARSER:', parser2.parse_args(args)
