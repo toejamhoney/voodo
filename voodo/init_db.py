deec48ee570e5d34831c7d5678693192e9ffc55c
ToeJam <toejamhoney@gmail.com>
2014-05-27 16:00:08 -0400

    Added sample_path settings

--- voodo/init_db.py
+++ voodo/init_db.py
@@ -57,7 +57,7 @@ Create tables
 '''
 tables = []
 tables.append("CREATE TABLE samples (sample_id INTEGER, path TEXT UNIQUE, name TEXT, set_id INTEGER, state INTEGER, finished_at DATETIME, PRIMARY KEY(sample_id))")
-tables.append("CREATE TABLE sample_sets (set_id INTEGER, set_name TEXT UNIQUE, PRIMARY KEY(set_id))")
+tables.append("CREATE TABLE sample_sets (set_id INTEGER, set_name TEXT UNIQUE, set_path TEXT, PRIMARY KEY(set_id))")
 tables.append("CREATE TABLE jobs (job_id INTEGER, job_name TEXT UNIQUE, set_id INTEGER, dictionary TEXT, state INTEGER, finished_at DATETIME, PRIMARY KEY(job_id))")
 tables.append("CREATE TABLE results (job_id INTEGER, sample_id INTEGER, results TEXT, PRIMARY KEY(job_id, sample_id))")
 for create in tables:
